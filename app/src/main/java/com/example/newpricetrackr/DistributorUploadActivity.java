07e00e18fef77de368a9e844e7ab01fbecc90001
AtiixAtiix <Atiix@mail.com>
2023-11-19 14:00:07 +0100

    Added DistributorUploadActivitx

---
+++ app/src/main/java/com/example/newpricetrackr/DistributorUploadActivity.java
@@ -0,0 +1,14 @@
+package com.example.newpricetrackr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DistributorUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distributor_upload);
+    }
+}
--- app/src/main/java/com/example/newpricetrackr/ManualActivity.java
+++ app/src/main/java/com/example/newpricetrackr/ManualActivity.java
@@ -2,13 +2,30 @@ package com.example.newpricetrackr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ManualActivity extends AppCompatActivity {
 
+    Button uploadItemBtn;
+    Button uploadDistributorBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+
+        uploadItemBtn = findViewById(R.id.uploadItemBtn);
+        uploadDistributorBtn = findViewById(R.id.uploadDistributorBtn);
+
+        uploadItemBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ManualActivity.this, ItemUploadActivity.class);
+            startActivity(intent);
+        });
+
+        uploadDistributorBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(ManualActivity.this, DistributorUploadActivity.class);
+            startActivity(intent);
+        });
     }
 }
